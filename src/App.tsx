31f44ad468cd73467b84d1938d14be53d30c4678
HubertNastaly <56291339+HubertNastaly@users.noreply.github.com>
2023-05-23 19:27:16 +0200

    Fetch project data from api (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-function App() {
+import { useState } from "react"
+import { useProject } from "./hooks"
+
+const App = () => {
+  const [projectId, setProjectId] = useState('')
+  const { loadProject, isLoading, error } = useProject()
+
   return (
     <>
-      Hello world
+      <input value={projectId} onChange={event => setProjectId(event.target.value)} />
+      <button onClick={() => loadProject(projectId)}>Load project</button>
+      {isLoading && <span>Loading...</span>}
+      {error && <span>Error: {error.message}</span>}
     </>
   )
 }
