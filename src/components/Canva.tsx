413c8fb3d73930b9142675429f052253567678e6
HubertNastaly <56291339+HubertNastaly@users.noreply.github.com>
2023-05-24 19:29:32 +0200

    Display bounding box for rectangles (#6)

--- src/components/Canva.tsx
+++ src/components/Canva.tsx
@@ -1,5 +1,6 @@
 import { Project } from "../types"
 import { getContrastColor } from "../utils/getContrastColor"
+import { BoundingBox } from "./BoundingBox"
 import { Pivot } from "./Pivot"
 import { Shape } from "./Shape"
 
@@ -14,10 +15,13 @@ export const Canva = ({ project }: Props) => {
       <span id="projectName">{name} (id: { id })</span>
       <svg id="canva" viewBox={`0 0 ${width} ${height}`}>
         {items.map(shape => (
-          <Shape key={shape.id} {...shape} />
+          <Shape key={`shape-${shape.id}`} {...shape} />
         ))}
         {items.map(({ id, x, y, color, rotation }) => (
-          <Pivot key={id} x={x} y={y} color={getContrastColor(color)} rotation={rotation}  />
+          <Pivot key={`pivot-${id}`} x={x} y={y} color={getContrastColor(color)} rotation={rotation}  />
+        ))}
+        {items.map(shape => (
+          <BoundingBox key={`bounding-box-${shape.id}`} shape={shape} />
         ))}
       </svg>
     </div>
