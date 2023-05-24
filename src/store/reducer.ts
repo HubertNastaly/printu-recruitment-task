a459eecc775f38d28b6352bf673c357cbc952272
HubertNastaly <h.nastaly@gmail.com>
2023-05-24 10:40:36 +0200

    Create store and reducer for project

---
+++ src/store/reducer.ts
@@ -0,0 +1,19 @@
+import { PayloadAction } from "@reduxjs/toolkit";
+import { Project } from "../schemas/project";
+
+export interface Store {
+  project?: Project
+}
+
+export const DEFAULT_STATE: Store = {}
+
+export type SetProjectAction = PayloadAction<Project, 'SET_PROJECT'>
+
+export function reducer(state: Store = DEFAULT_STATE, action: SetProjectAction): Store {
+  switch(action.type) {
+    case 'SET_PROJECT':
+      return { project: action.payload }
+    default:
+      return state
+  }
+}
