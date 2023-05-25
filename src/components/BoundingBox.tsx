a66bad25fcbaa304b8fe40539cf216daeb1af1f2
HubertNastaly <h.nastaly@gmail.com>
2023-05-25 15:30:24 +0200

    Display bounding box for ellipses

--- src/components/BoundingBox.tsx
+++ src/components/BoundingBox.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react"
 import { ProjectItem } from "../types"
 import { calculateRectangleVertices, rotatePoint } from "../utils"
+import { toRadians } from "../utils/toRadians"
 
 interface Props {
   shape: ProjectItem
@@ -11,7 +12,7 @@ export const BoundingBox = ({ shape }: Props) => {
     case 'rectangle':
       return <RectangleBoundingBox shape={shape} />
     case 'ellipse':
-      return <></>
+      return <EllipseBoundingBox shape={shape} />
   }
 }
 
@@ -43,5 +44,54 @@ function calculateRectangleBoundingBox({ x, y, width, height, rotation }: Projec
     yMin: Math.min(...yValues),
     xMax: Math.max(...xValues),
     yMax: Math.max(...yValues)
+  }
+}
+
+const EllipseBoundingBox = ({ shape }: Props) => {
+  const { color } = shape
+  const { xMin, xMax, yMin, yMax } = useMemo(() => calculateEllipseBoundingBox(shape), [shape])
+
+  return (
+    <rect
+      x={xMin}
+      y={yMin}
+      width={xMax - xMin}
+      height={yMax - yMin}
+      fill="none"
+      strokeWidth={1}
+      stroke={color}
+    />
+  )
+}
+
+function calculateEllipseBoundingBox({ x: xCenter, y: yCenter, width, height, rotation }: ProjectItem) {
+  const { sin, cos, tan, atan, PI } = Math
+  const rotationInRadians = toRadians(rotation)
+
+  const xRadius = width / 2
+  const yRadius = height / 2
+
+  const sinRotation = sin(rotationInRadians)
+  const cosRotation = cos(rotationInRadians)
+
+  // https://en.wikipedia.org/wiki/Ellipse, look: Standard parametric representation
+  // xRotated(t) = x(t) * cos(rotation) - y(t) * sin(rotation)
+  // xRotated(t) = xRadius * cos(rotation) * cos(t) - yRadius * sin(rotation) * sin(t)
+  const xRotated = (radians: number) => xRadius * cosRotation * cos(radians) - yRadius * sinRotation * sin(radians)
+  const yRotated = (radians: number) => xRadius * sinRotation * cos(radians) + yRadius * cosRotation * sin(radians)
+
+  // xRotated'(t) = -xRadius * cos(rotation) sin(t) - yRadius * sin(rotation) * cos(t)
+  // xRotated'(t) = 0  =>  tg(t) = -(yRadius / xRadius) * tg(rotation)
+  const xExtremeAngle = atan(-yRadius / xRadius * tan(rotationInRadians))
+  const yExtremeAngle = atan(yRadius / xRadius / tan(rotationInRadians))
+
+  const [xMin, xMax] = [xRotated(xExtremeAngle), xRotated(xExtremeAngle + PI)].map(x => x + xCenter).sort()
+  const [yMin, yMax] = [yRotated(yExtremeAngle), yRotated(yExtremeAngle + PI)].map(y => y + yCenter).sort()
+
+  return {
+    xMin,
+    yMin,
+    xMax,
+    yMax
   }
 }
--- src/utils/rotatePoint.ts
+++ src/utils/rotatePoint.ts
@@ -1,9 +1,10 @@
 import { Point } from "../types"
+import { toRadians } from "./toRadians"
 
 export function rotatePoint(point: Point, pivot: Point, degrees: number): Point {
   const vector: Point = [point[0] - pivot[0], point[1] - pivot[1]]
 
-  const radians = degrees * Math.PI / 180
+  const radians = toRadians(degrees)
   const sinus = Math.sin(radians)
   const cosinus = Math.cos(radians)
 
